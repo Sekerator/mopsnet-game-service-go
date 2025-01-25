5801d30355d82d72f431e2f5ae69969d4dfea33a
Taghi Eynullaev <55533028+Sekerator@users.noreply.github.com>
2025-01-25 21:18:51 +0300

    First commit

---
+++ cmd/main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"game-service/config"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	logFile, err := os.OpenFile("logs/requests.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("Ошибка открытия файла для логов: %v", err)
+	}
+	defer logFile.Close()
+
+	logger := log.New(logFile, "", log.LstdFlags)
+	cfg, err := config.FromEnv()
+	if err != nil {
+		log.Fatalf("Ошибка загрузки данных: %v", err)
+		return
+	}
+
+	conn, err := sqlx.Connect("postgres", cfg.DatabaseUrl)
+	if err != nil {
+		log.Fatalf("Ошибка подключения к БД: %v", err)
+		return
+	}
+	defer conn.Close()
+
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.RequestLogger(&middleware.DefaultLogFormatter{
+		Logger:  logger,
+		NoColor: true,
+	}))
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(5 * time.Second))
+
+	//userRepository := repositories.NewUserRepo(conn)
+	//userService := services.NewUserService(userRepository)
+	//userHandler := handlers.NewUserHandler(userService)
+
+	r.Route("/user", func(r chi.Router) {
+		//r.Post("/login", userHandler.Login)
+	})
+
+	log.Println("Сервер запущен по адресу: ", cfg.ListenAddrAndPort())
+	log.Println("Версия: ", cfg.Env)
+	logger.Println("Сервер запущен по адресу: ", cfg.ListenAddrAndPort())
+	logger.Println("Версия: ", cfg.Env)
+
+	if cfg.Env == "local" {
+		err = http.ListenAndServe(cfg.ListenAddrAndPort(), r)
+	} else {
+		go func() {
+			log.Println("Redirecting HTTP to HTTPS")
+			_ = http.ListenAndServe(":8080", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				http.Redirect(w, r, "https://"+r.Host+r.RequestURI, http.StatusMovedPermanently)
+			}))
+		}()
+
+		err = http.ListenAndServeTLS(cfg.ListenAddrAndPort(),
+			"/etc/letsencrypt/live/mopsnet.ru/fullchain.pem",
+			"/etc/letsencrypt/live/mopsnet.ru/privkey.pem",
+			r)
+	}
+
+	if err != nil {
+		logger.Fatalf("Ошибка запуска сервера: %v", err)
+		return
+	}
+
+	log.Println("Сервер отключен")
+	logger.Println("Сервер отключен")
+}
---
+++ config/cfg.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	ListenAddr  string `mapstructure:"LISTEN_ADDR"`
+	ListenPort  string `mapstructure:"LISTEN_PORT"`
+	Env         string `mapstructure:"ENV"`
+	DatabaseUrl string `mapstructure:"DATABASE_URL"`
+	RedisUrl    string `mapstructure:"REDIS_URL"`
+	RedisPass   string `mapstructure:"REDIS_PASS"`
+}
+
+func (c Config) ListenAddrAndPort() string {
+	return fmt.Sprintf("%s:%s", c.ListenAddr, c.ListenPort)
+}
+
+func FromEnv() (*Config, error) {
+	v := viper.New()
+	v.SetDefault("LISTEN_ADDR", "0.0.0.0")
+	v.SetDefault("LISTEN_PORT", "8080")
+	v.SetDefault("ENV", "local")
+	v.SetDefault("DATABASE_URL", "postgres://postgres:postgres@localhost:5432/magbat_user?sslmode=disable")
+	v.SetDefault("REDIS_URL", "localhost:6379")
+	v.SetDefault("REDIS_PASS", "45554555")
+	v.SetConfigName("env")
+	v.SetConfigFile("../.env")
+	_ = v.ReadInConfig()
+	v.AutomaticEnv()
+
+	cfg := Config{}
+	err := v.Unmarshal(&cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return &cfg, nil
+}
